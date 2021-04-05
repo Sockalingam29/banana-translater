16799fa9d4c7d4db4c0ae82bdf2849424d7f69cb
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2021-04-05 20:28:37 +0530

    final touches done

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-var btnTranslate=document.querySelector("#btntranslate");
-var txt=document.querySelector("#text");
-var outputDiv=document.querySelector("#outputdiv");
-var url="https://api.funtranslations.com/translate/pirate.json";
+var btnTranslate = document.querySelector("#btntranslate");
+var txt = document.querySelector("#text");
+var outputDiv = document.querySelector("#outputdiv");
+var url = "https://api.funtranslations.com/translate/pirate.json";
 
-function clickhandler()
-{
+function clickhandler() {
    console.log("clicked");
-   var input=txt.value;
+   var input = txt.value;
    console.log(input);
-   var finalurl=constructUrl(input);
+   var finalurl = constructUrl(input);
    console.log(finalurl);
-   fetch (finalurl)
-   .then(response=>response.json())
-   .then(json => { 
-      outputDiv.innerText=json.contents.translated;
-   })
-   .catch(() => {
-      alert("Some error occured");
-   })
+   fetch(finalurl)
+      .then(response => response.json())
+      .then(json => {
+         outputDiv.innerText = json.contents.translated;
+      })
+      .catch(() => {
+         alert("Some error occured");
+      })
 
 };
 
-btnTranslate.addEventListener("click",clickhandler);
+btnTranslate.addEventListener("click", clickhandler);
 
-function constructUrl(inputtxt){
-   var encoded=encodeURI(inputtxt);
+function constructUrl(inputtxt) {
+   var encoded = encodeURI(inputtxt);
    return `${url}?text=${encoded}`;
 };
