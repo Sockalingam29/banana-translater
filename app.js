2f35186d0a86737d61b667abf57b6a440a8fa76e
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2021-04-05 14:47:25 +0530

    pending: minor bug fixes(padding, margin, etc.)

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-console.log("Hi")
 var btnTranslate=document.querySelector("#btntranslate");
-var txt=document.querySelector("#txt");
-function myFunc()
+var txt=document.querySelector("#text");
+var outputDiv=document.querySelector("#outputdiv");
+var url="https://api.funtranslations.com/translate/pirate.json";
+
+function clickhandler()
 {
-   console.log("HI");
-   console.log("https://api.funtranslations.com/translate/")
-}
+   console.log("clicked");
+   var input=txt.value;
+   console.log(input);
+   var finalurl=constructUrl(input);
+   console.log(finalurl);
+   fetch (finalurl)
+   .then(response=>response.json())
+   .then(json => { 
+      outputDiv.innerText=json.contents.translated;
+   })
+   .catch(() => {
+      alert("Some error occured");
+   })
+
+};
+
+btnTranslate.addEventListener("click",clickhandler);
+
+function constructUrl(inputtxt){
+   var encoded=encodeURI(inputtxt);
+   return `${url}?text=${encoded}`;
+};
