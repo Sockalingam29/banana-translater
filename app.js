dd550b3a63cc1909c233f27a651368182c9c0059
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2021-04-05 11:24:14 +0530

    structure of website

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Hi")
+var btnTranslate=document.querySelector("#btntranslate");
+var txt=document.querySelector("#txt");
+function myFunc()
+{
+   console.log("HI");
+   console.log("https://api.funtranslations.com/translate/")
+}
